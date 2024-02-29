f1de9a2bd6914cc21fb53b13138fc3d5c63be179
sasineelakandan <devasasi887@gmail.com>
2024-02-29 20:32:17 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const express=require('express')
+const session=require('express-session')
+const data=require('./data')
+const app=express()
+const credentials={email:'devasasi@gmail.com',password:407256}
+
+app.set('view engine','ejs')
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true,
+    
+  }))
+
+app.use((req, res, next) => {
+    res.set("Cache-Control", "no-store");
+    next();
+});
+
+
+app.get('/',(req,res)=>{
+    if(req.session.log){
+        res.render('home',{data})
+    }else{
+        res.render('login',{invalid:req.session.invalid})
+        req.session.invalid=false
+        req.session.save()
+        
+    }
+
+})
+
+app.post('/login',(req,res)=>{
+    if(req.body.email==credentials.email && req.body.password==credentials.password){
+        req.session.log=true
+        
+         res.redirect('/')
+    }else{
+        req.session.invalid=true
+        res.redirect('/')
+    }
+})
+
+app.post('/home',(req,res)=>{
+    req.session.log=false
+    res.redirect('/')
+})
+
+app.listen(5000,()=>{
+    console.log('portstarted')
+})
---
+++ data.js
@@ -0,0 +1,23 @@
+const data=[
+    {
+        name:'sasi',
+        age:24,
+        mark:98
+    },
+    {
+        name:'jithin',
+        age:25,
+        mark:99
+    },
+    {
+        name:'yasir',
+        age:22,
+        mark:100
+    },
+    {
+        name:'riyas',
+        age:24,
+        mark:96
+    }
+]
+module.exports= data
